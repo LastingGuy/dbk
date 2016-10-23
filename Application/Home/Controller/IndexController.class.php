2e7932efce51d2347b686bbfbc87f9a2a8dd120c
BEN <wjlwjl1996@outlook.com>
2016-10-23 21:57:45 +0800

    数据库
    merge

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -12,14 +12,17 @@ class IndexController extends Controller{
         $this->display();
     }
 
-    public function newOrder()
+    #create the order of receiving mail
+    public function newRecvOrder()
 	{
-        if(IS_POST)
+        if(true)
         {
             $data = I('post.');
             $school = $data['school'];
             $city = $data['city'];
             $address = $data['address'];
+
+            ///获得寝室id
             $DOR = D('DormitoryView'); //实例化寝室模型
             $dor = $DOR->field('dormitory_id')->where("school_name='$school' and school_city='$city' and dormitory_address = '$address'")->select();
             if(count($dor)>0)
@@ -32,6 +35,7 @@ class IndexController extends Controller{
                 $this->ajaxReturn('请填写正确的收货人地址');
             }
 
+            //写入数据库
             $data['receiver_name'] = $data['rename'];
             $data['receiver_phone'] = $data['tel'];
             $data['dormitory_id'] = $data['dor'];
@@ -66,5 +70,64 @@ class IndexController extends Controller{
             $this->ajaxReturn('提交失败');
         }
     }
+
+    public function newSendOrder()
+    {
+        if(IS_POST)
+        {
+            $send = D('send');
+            $data = I('post.');
+
+            $school = $data['school'];
+            $city = $data['city'];
+            $address = $data['address'];
+            // $data['rename'] = '123';
+            // $data['tel'] = 123;
+            // $data['dor'] = 1;
+            // $data['delivery'] = 'gum';
+            // $data['remark'] = 'no';
+
+            ///获得寝室id
+            $DOR = D('DormitoryView'); //实例化寝室模型
+            $dor = $DOR->field('dormitory_id')->where("school_name='$school' and school_city='$city' and dormitory_address = '$address'")->select();
+            if(count($dor)>0)
+            {
+                $dor = $dor[0]['dormitory_id'];
+                $data['dor'] = $dor;
+            }
+            else
+            {
+                $this->ajaxReturn('请填写正确的收货人地址');
+            }
+
+            ///写入数据库
+            $data['sender_name'] = $data['rename'];
+            $data['sender_phone'] = $data['tel'];
+            $data['dormitory_id'] = $data['dor'];
+            $data['send_goods'] = $data['delivery'];
+            $data['time'] = date('Y-m-d H:i:s');
+            // var_dump($data);
+            if($send->create($data))
+            {
+                if($send->add($data))
+                {
+                    print('提交成功');
+                }
+                else
+                {
+                    print('提交失败');
+                }
+            }
+            else
+            {
+                print($send->getError());
+            }
+        }
+        else
+        {
+           print('提交失败！');
+        }
+    }
+
 }
 ?>
---
+++ Application/Home/Model/SendModel.class.php
@@ -0,0 +1,22 @@
+<?php
+namespace Home\Model;
+use Think\Model;
+
+class SendModel extends Model
+{
+    // protected $_map = array(
+
+    //     'rename' => 'sender_name',
+    //     'tel' => "sender_phone",
+    //     'dor' => 'dormitory_id',
+    //     'delivery' => 'send_goods'
+    // );
+
+    protected $_validate = array(
+        array('sender_name','require','请输入发件人姓名'),
+        array('sender_phone','require','请输入发件人电话'),
+        array('dormitory_id','require','请填写正确的发件人地址'),
+        array('send_goods','require','请填写寄件物品')
+    );
+}
+?>
