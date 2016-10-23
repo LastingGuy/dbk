c0d6d705b5447b079cac99bbccd89a5778ae9c06
ZhengChenYang <526789319@qq.com>
2016-10-23 22:57:41 +0800

    last one

--- Application/Admin/Common/AdminDAOImpl.class.php
+++ Application/Admin/Common/AdminDAOImpl.class.php
@@ -11,7 +11,7 @@ class AdminDAOImpl implements IAdminDAO{
     public function login($user_id, $user_passwd){
         $model = M("admin");
         $condition['admin_id'] = $user_id;
-        if($model->where($condition)->getField('admin_passwd') == $user_passwd){
+        if($model->where($condition)->getField('admin_passwd') === $user_passwd){
             $this->addSession($user_id);
             return  1;
         }else{
