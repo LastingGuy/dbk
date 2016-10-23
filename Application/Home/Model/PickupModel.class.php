e039e70f02f890161c5595929c2ca4aae9baefce
BEN <wjlwjl1996@outlook.com>
2016-10-23 23:20:51 +0800

    修改js css 路径

--- Application/Home/Model/PickupModel.class.php
+++ Application/Home/Model/PickupModel.class.php
@@ -3,14 +3,14 @@ namespace Home\Model;
 use Think\Model;
 class PickupModel extends Model
 {
-    protected $_map = array(
+    // protected $_map = array(
 
-        'rename' => 'receiver_name',
-        'tel' => 'receiver_phone',
-        'dor' => 'dormitory_id',
-        'express' => 'express_company',
-        'fetch_code' => 'express_code',
-    );
+    //     'rename' => 'receiver_name',
+    //     'tel' => 'receiver_phone',
+    //     'dor' => 'dormitory_id',
+    //     'express' => 'express_company',
+    //     'fetch_code' => 'express_code',
+    // );
 
     protected $_validate = array(
         array('receiver_name','require','请输入收件人姓名'),
