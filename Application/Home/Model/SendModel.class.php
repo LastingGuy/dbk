758ba604eaa76650baf2992e24526a0d2bf688f0
BEN <wjlwjl1996@outlook.com>
2016-10-27 16:53:01 +0800

    寄件地址

--- Application/Home/Model/SendModel.class.php
+++ Application/Home/Model/SendModel.class.php
@@ -17,6 +17,7 @@ class SendModel extends Model
         array('sender_phone','require','请输入发件人电话'),
         array('dormitory_id','require','请填写正确的发件人地址'),
         array('sender_goods','require','请填写寄件物品'),
+        array('destination','require','请填写寄件地址'),
         array('openid','require','请登录！')
     );
 }
