b5dc3544941092066885834d84b9a71e2d1b7f8d
BEN <wjlwjl1996@outlook.com>
2016-11-24 16:16:10 +0800

    213

--- Application/Home/Common/OrderDAOlmpl.class.php
+++ Application/Home/Common/OrderDAOlmpl.class.php
@@ -416,11 +416,18 @@ class OrderDAOlmpl implements IOrderDAO
     //判断是否为手机号码
     private function isMobile($mobile) 
     {
+        $is_tel = preg_match("/^([0-9]{3,4}-)?[0-9]{7,8}$/",$mobile)?true:false;
+        if($is_tel)
+        {
+            return true;
+        }
+
         if (!is_numeric($mobile)) 
         {
             return false;
-        }
-        return preg_match('#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,7,8]{1}\d{8}$|^18[\d]{9}$#', $mobile) ? true : false;
+        }     
+        $is_mobile =preg_match('#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,7,8]{1}\d{8}$|^18[\d]{9}$#', $mobile) ? true : false;
+        return $is_mobile;
     }
 
     //验证是否存在相同订单
