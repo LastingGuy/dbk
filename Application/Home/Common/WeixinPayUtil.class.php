8825d8e0a26ea669db2bb899b1058a0b7aaa3e0c
BEN <wjlwjl1996@outlook.com>
2016-11-25 17:10:49 +0800

    321

--- Application/Home/Common/WeixinPayUtil.class.php
+++ Application/Home/Common/WeixinPayUtil.class.php
@@ -171,7 +171,7 @@ class WeixinPayUtil{
                     $refund['pay_type'] = $pay['pay_type'];
                     $refund['trade_no'] = $pay['trade_no'];
                     $refund['total_fee'] = $pay['total_fee'];
-                    $refund['refund_fee'] = $pay['settlement_refund_fee'];
+                    $refund['refund_fee'] = $result['settlement_refund_fee'];
                     $refund['refund_time'] = date('Y-m-d H:i:s');
                     $refund['refund_id'] = $result['refund_id'];
                     $mod->add($refund);                  
