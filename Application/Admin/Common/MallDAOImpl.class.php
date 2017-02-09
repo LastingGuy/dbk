31afa9c3eab035326422e8316d5ce0bc5d7ea8c5
ZhengChenYang <526789319@qq.com>
2017-02-09 17:25:26 +0800

    1

--- Application/Admin/Common/MallDAOImpl.class.php
+++ Application/Admin/Common/MallDAOImpl.class.php
@@ -13,13 +13,13 @@ class MallDAOImpl
     {
         $return_data['draw'] = $param["draw"];
         $model = M("goods_view");
-        $return_data['recordsTotal'] = $model->order("goods_time desc")->limit($param['start'],$param['length'])->count();
+        $return_data['recordsTotal'] = $model->order("goods_time desc")->limit($param['start'], $param['length'])->count();
         $return_data['recordsFiltered'] = $return_data['recordsTotal'];
-        $return_data['data'] = $model->order("goods_time desc")->limit($param['start'],$param['length'])->select();
+        $return_data['data'] = $model->order("goods_time desc")->limit($param['start'], $param['length'])->select();
 
-        foreach($return_data['data'] as $key=>$value){
+        foreach ($return_data['data'] as $key => $value) {
             $return_data['data'][$key]['edit'] = "";
-            $return_data['data'][$key]['look'] = "<img src='".__ROOT__."/Public/assets/advanced-datatable/examples/examples_support/details_open.png'>";
+            $return_data['data'][$key]['look'] = "<img src='" . __ROOT__ . "/Public/assets/advanced-datatable/examples/examples_support/details_open.png'>";
         }
         return $return_data;
     }
@@ -27,14 +27,29 @@ class MallDAOImpl
     public function update($param)
     {
         $model = M("goods");
-        $model->create();
-        $model->save();
+        return $model->save($param);
     }
 
     public function add($param)
     {
+        $count = 0;
         $model = M("goods");
-        $model->add($param);
+        $param['goods_id'] = 1;
+        $param['goods_page_view'] = 0;
+        $param['goods_online'] = 0;
+        $param['goods_time'] = date("Y-m-d H:i:s", time());
+        do{
+            $result = 0;
+            try{
+                $result = $model->add($param);
+            }
+            catch(\Exception $e){
+                $param['goods_id'] = $this->buildId();
+                $count++;
+                if($count>10)
+                    return false;
+            }
+        }while($result==0);
     }
 
     //获得一级目录
@@ -52,12 +67,33 @@ class MallDAOImpl
     }
 
     //
-    public function updateOnline($goods_id, $goods_online){
+    public function updateOnline($goods_id, $goods_online)
+    {
 
         $array['goods_id'] = $goods_id;
         $array['goods_online'] = $goods_online;
 
         $model = M("goods");
         $model->save($array);
+    }
+
+    public function getOneGoods($goods_id)
+    {
+        $model = M("goodsView");
+        return $model->find($goods_id);
+    }
+
+    //随机生成6位带字母数字的goods_id
+    private function buildId()
+    {
+        $arr = array_merge(range(0, 9), range('a', 'z'));
+
+        $str = '';
+        $arr_len = count($arr);
+        for ($i = 0; $i < 6; $i++) {
+            $rand = mt_rand(0, $arr_len - 1);
+            $str .= $arr[$rand];
+        }
+        return $str;
     }
 }
--- Application/Admin/Controller/MallController.class.php
+++ Application/Admin/Controller/MallController.class.php
@@ -39,6 +39,18 @@ class MallController extends Controller
 
     }
 
+    //得到指定的信息
+    public function getOneGoods()
+    {
+        if(!session("?admin_id")) {
+            header('Location:'.U("Admin/Index/index"));
+        }
+        $goods_id = I("get.goods_id");
+        $object = new Common\MallDAOImpl();
+        $return_data = $object->getOneGoods($goods_id);
+        $this->ajaxReturn($return_data);
+    }
+
     //编辑商品
     public function update()
     {
@@ -46,8 +58,15 @@ class MallController extends Controller
             header('Location:'.U("Admin/Index/index"));
         }
 
+        $param['goods_id'] = I("post.goods_id");
+        $param['goods_name'] = I("post.goods_name");
+        $param['goods_price'] = I("post.goods_price");
+        $param['goods_link'] = I("post.goods_link");
+        $param['goods_description'] = I("post.goods_description");
+        $param['classify2_id'] = I("post.classify2_id");
+
         $object = new Common\MallDAOImpl();
-        $object->update(null);
+        $this->ajaxReturn($object->update($param));
 
     }
 
@@ -57,6 +76,17 @@ class MallController extends Controller
         if(!session("?admin_id")) {
             header('Location:'.U("Admin/Index/index"));
         }
+
+        $param['goods_name'] = I("post.goods_name");
+        $param['goods_price'] = I("post.goods_price");
+        $param['goods_link'] = I("post.goods_link");
+        $param['goods_description'] = I("post.goods_description");
+        $param['classify2_id'] = I("post.classify2_id");
+        if($param['classify2_id']=="")
+            $param['classify2_id'] = null;
+
+        $object = new Common\MallDAOImpl();
+        $this->ajaxReturn($object->add($param));
     }
 
     //增加商品
